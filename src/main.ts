cf3b0465cbb4a6137e6b04a9ab2aba20b36afdd6
corebari <corebari@outlook.com>
2023-03-09 14:10:05 -0500

    feat: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import * as expressBasicAuth from 'express-basic-auth';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Order Management API')
+    .setDescription(
+      'This API service is for order management in billbee and szamla',
+    )
+    .setVersion('1.0')
+    .build();
+
+  app.use(
+    '/doc',
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        szamlazz: 'szamlazz',
+      },
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
